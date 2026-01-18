a2807d2f690fc6aff9ddf8a2f4339a9f095f53c7
Ankit <ankit@choudharifamily.com>
2026-01-17 17:08:34 -0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-=======
-   import ReactDOM from 'react-dom/client';
-   import './index.css';
-   import App from './App';
->>>>>>> 74717664b11056b95c9422558383bcaf7f2a1188
 
-   const root = ReactDOM.createRoot(document.getElementById('root'));
-   root.render(
-     <React.StrictMode>
-       <App />
-     </React.StrictMode>
-   );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
